4bc2ce24e1e74ec39bb251b3dcd3d982d74388c4
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2021-11-01 23:07:31 +1100

    revert

--- keystone.ts
+++ keystone.ts
@@ -39,7 +39,7 @@ export default withAuth(
         ],
       ],
       // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => true,
+      isAccessAllowed: (context) => !!context.session?.data,
     },
 
     lists,
