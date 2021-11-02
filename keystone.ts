ab9e18ccd9715562456d434f737e28a81fc3f471
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2021-11-03 09:01:17 +1100

    add startswith check

--- keystone.ts
+++ keystone.ts
@@ -5,16 +5,13 @@ It looks at the default export, and expects a Keystone config object.
 
 You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
 */
-import redis from "redis";
 import { config } from "@keystone-next/keystone";
-import { redisSessionStore } from "@keystone-next/session-store-redis";
-// Look in the schema file for how we define our lists, and how users interact with them through graphql or the Admin UI
 import lists from "./schema";
 
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from "./auth";
-import { storedSessions } from "@keystone-next/keystone/session";
 import express from "express";
+import { startsWith } from "lodash";
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
@@ -36,14 +33,15 @@ export default withAuth(
           {
             mode: "write",
             outputPath: "next.config.js",
-            src: `module.exports = { ...require('@keystone-next/keystone/static/next.config.js'), images: { domains: ["pbs.twimg.com"], disableStaticImages: true } }`,
+            src: `module.exports = { ...require('@keystone-next/keystone/static/next.config.js'), images: { domains: ["pbs.twimg.com"]} }`,
           },
         ],
       ],
       // For our starter, we check that someone has session data before letting them see the Admin UI.
       isAccessAllowed: (context) => {
-        console.log(context);
-        return !!context.session?.data;
+        return startsWith(context.req?.url, "/images")
+          ? true
+          : !!context.session?.data;
       },
     },
     graphql: {
